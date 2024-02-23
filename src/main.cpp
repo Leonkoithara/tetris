78d3bbf498d8ac5a3a7c859d26fa0c8b57dcac3a
Leon Joe K <36569141+Leonkoithara@users.noreply.github.com>
2024-02-23 23:56:04 +0530

    Make blocks fall

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,28 @@
+#include "SDL_render.h"
+#include <chrono>
+#include <cstdlib>
 #include <iostream>
 #include <vector>
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_ttf.h>
 
+
 bool running;
+bool falling = true;
+int block_size = 48;
 int score = 0;
+int screen_x = 480;
+int screen_y = 960;
+std::vector<std::vector<SDL_Rect>> shapes;
+std::vector<std::pair<int, int>> shape_positions;
+std::vector<SDL_Rect> falling_shape;
 
 void process_events();
 std::vector<SDL_Rect> get_random_shape();
-void render_shape_at_pos(SDL_Renderer *renderer, std::vector<SDL_Rect> shape, int x, int y);
+void render_shape_at_pos(SDL_Renderer *renderer, std::vector<SDL_Rect> shape,
+                         int x, int y);
+void update_falling_blocks();
+void spawn_new();
 
 int main()
 {
@@ -25,18 +39,49 @@ int main()
         exit(1);
     }
 
-    SDL_Window *win = SDL_CreateWindow("Tetris", 10, 10, 480, 960, 0);
+    SDL_Window *win = SDL_CreateWindow("Tetris", 10, 10, screen_x, screen_y, 0);
     SDL_Renderer *renderer = SDL_CreateRenderer(win, -1, 0);
     SDL_DisplayMode DM;
     SDL_GetCurrentDisplayMode(0, &DM);
     float width = DM.w;
     float height = DM.h;
     
+    auto old_time = std::chrono::high_resolution_clock::now();
+    int block_speed = 1; // falls 1 block every 1 second
+
+    falling_shape = get_random_shape();
+    shapes.push_back(falling_shape);
+    shape_positions.push_back(std::pair<int, int>(0, 0));
+
     while (running)
 	{
+        auto new_time = std::chrono::high_resolution_clock::now();
+        auto diff = std::chrono::duration_cast<std::chrono::seconds>(new_time - old_time);
+        if (diff.count() >= block_speed)
+        {
+            update_falling_blocks();
+            old_time = new_time;
+        }
+
         SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
         SDL_RenderClear(renderer);
         SDL_RenderDrawRect(renderer, NULL);
+
+        for (int i=0; i<shapes.size(); i++)
+		{
+            SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+            for (SDL_Rect rectangle : shapes[i])
+			{
+                rectangle.x += shape_positions[i].first;
+                rectangle.y += shape_positions[i].second;
+                rectangle.x *= block_size;
+                rectangle.y *= block_size;
+                rectangle.w *= block_size;
+                rectangle.h *= block_size;
+
+                SDL_RenderFillRect(renderer, &rectangle);
+            }
+        }
         SDL_RenderPresent(renderer);
         process_events();
     }
@@ -61,39 +106,43 @@ void process_events()
 
 std::vector<SDL_Rect> get_random_shape()
 {
-    std::vector<SDL_Rect> I_block = {
-        { 0, 0, 1, 4 }
-    };
-    std::vector<SDL_Rect> L_block = {
-        { 0, 0, 1, 1 },
-        { 0, 1, 3, 1 }
-    };
-    std::vector<SDL_Rect> J_block = {
-        { 0, 1, 3, 1 },
-        { 2, 0, 1, 1 }
-    };
-    std::vector<SDL_Rect> O_block = {
-        { 0, 0, 2, 2 }
-    };
-    std::vector<SDL_Rect> S_block = {
-        { 0, 1, 2, 1 },
-        { 1, 0, 2, 1 }
-    };
-    std::vector<SDL_Rect> T_block = {
-        { 0, 1, 3, 1 },
-        { 1, 0, 1, 1 }
-    };
-    std::vector<SDL_Rect> Z_block = {
-        { 0, 0, 2, 1 },
-        { 1, 1, 2, 1 }
+    std::vector<std::vector<SDL_Rect>> blocks = {
+        {
+            { 0, 0, 1, 4 }
+        },
+        {
+            { 0, 0, 1, 1 },
+            { 0, 1, 3, 1 }
+        },
+        {
+            { 2, 0, 1, 1 },
+            { 0, 1, 3, 1 }
+        },
+        {
+            { 0, 0, 2, 1 },
+            { 0, 1, 2, 1 }
+        },
+        {
+            { 1, 0, 2, 1 },
+            { 0, 1, 2, 1 }
+        },
+        {
+            { 1, 0, 1, 1 },
+            { 0, 1, 3, 1 }
+        },
+        {
+            { 0, 0, 2, 1 },
+            { 1, 1, 2, 1 }
+        }
     };
 
-    return Z_block;
+    int rand_num = rand()%7;
+
+    return blocks[rand_num];
 }
 
 void render_shape_at_pos(SDL_Renderer *renderer, std::vector<SDL_Rect> shape, int x, int y)
 {
-    int block_size = 48;
     for (SDL_Rect rectangle : shape)
 	{
         rectangle.x += x;
@@ -104,4 +153,36 @@ void render_shape_at_pos(SDL_Renderer *renderer, std::vector<SDL_Rect> shape, in
         rectangle.h *= block_size;
         SDL_RenderFillRect(renderer, &rectangle);
     }
+}
+
+void update_falling_blocks()
+{
+    int xb = falling_shape.back().x + shape_positions.back().first;
+    int wb = falling_shape.back().w;
+    int yb = falling_shape.back().y + shape_positions.back().second;
+    std::cout << xb << " " << wb << " " << yb << std::endl;;
+    for (int i=0; i<shape_positions.size(); i++)
+	{
+        int x = shapes[i].front().x + shape_positions[i].first;
+        int w = shapes[i].front().w;
+        int y = shapes[i].front().y + shape_positions[i].second;
+        if ((y == yb+1 && (xb <= x+w && xb+wb >= x)) || yb == 19)
+		{
+            std::cout << x << " " << w << " " << y << std::endl;
+            falling = false;
+            break;
+        }
+    }
+    if (falling)
+        shape_positions.back().second++;
+    else
+        spawn_new();
+}
+
+void spawn_new()
+{
+    falling = true;
+    falling_shape = get_random_shape();
+    shapes.push_back(falling_shape);
+    shape_positions.push_back(std::pair<int, int>(0, 0));
 }
